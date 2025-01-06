912e10aa5aa8a4b9d377d82d41f0f5df36585fd4
Hemant Kumar Saw <67191552+Soul-Hacker@users.noreply.github.com>
2025-01-06 18:00:13 +0530

    First Commit StartegyDesignPattern

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import StrategyDesignPattern.Using.Vehicle;
 public class Main {
     public static void main(String[] args) {
 // Uncomment this for StrategyDesign Pattern
-        Vehicle vh=new SportsVehicle();
+        Vehicle vh=new PassengerVehicle();
         vh.drive();
 
         System.out.println("Hello, World!");
