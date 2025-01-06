5f4cd0b0689b29977748fd2356f72d9cb52dff05
Hemant Kumar Saw <67191552+Soul-Hacker@users.noreply.github.com>
2025-01-06 17:59:48 +0530

    Fix for Ec-121

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ import StrategyDesignPattern.Using.Vehicle;
 public class Main {
     public static void main(String[] args) {
 // Uncomment this for StrategyDesign Pattern
-//        Vehicle vh=new SportsVehicle();
-//        vh.drive();
+        Vehicle vh=new SportsVehicle();
+        vh.drive();
 
         System.out.println("Hello, World!");
     }
