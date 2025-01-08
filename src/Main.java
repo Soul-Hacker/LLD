d95761c580c6d712e0522ef218227f6a47ca7003
Hemant Kumar Saw <67191552+Soul-Hacker@users.noreply.github.com>
2025-01-08 12:57:39 +0530

    minor fixing

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,5 @@ import StrategyDesignPattern.Using.Vehicle;
 
 public class Main {
     public static void main(String[] args) {
-// Uncomment this for StrategyDesign Pattern
-//        Vehicle vh=new SportsVehicle();
-//        vh.drive();
-
-        System.out.println("Hello, World!");
     }
 }
